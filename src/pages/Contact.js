fc9182238ae7b25bfa469dfccbff89e8537af15d
Hage, Andrew <ahage@iu.edu>
2024-12-01 17:42:25 -0500

    readme

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,12 +2,12 @@ import React from "react";
 import ContactForm from "../components/ContactForm";
 
 const Contact = () => {
-  return (
-    <div>
-      <h1 className="text-3xl font-semibold mb-6 text-center">Contact Us</h1>
-      <ContactForm />
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="text-3xl font-semibold mb-6 text-center">Contact Us</h1>
+            <ContactForm />
+        </div>
+    );
 };
 
 export default Contact;
