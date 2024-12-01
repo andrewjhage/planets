88d58d1c31ffa91c5ec911c979833e1689e74a78
Hage, Andrew <ahage@iu.edu>
2024-12-01 18:19:16 -0500

    ajh

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,25 +1,72 @@
+import React, { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import Header from "../components/Header";
 
 const Layout = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
-        <>
-        <Header fontFamily ="Arial" />
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/About">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/Contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-            <Outlet />
-        </>
+    <>
+        <Header fontFamily="Arial" />
+        <nav className="bg-gray-800 text-white">
+        <div className="flex items-center justify-between px-4 py-3 md:px-6">
+            <button
+            className="md:hidden block text-white focus:outline-none"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            >
+            <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d={isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
+                />
+            </svg>
+            </button>
+        </div>
+
+        {/* Menu Links */}
+        <div
+            className={`${
+                isMenuOpen ? "block" : "hidden"
+            } md:flex md:items-center md:justify-center md:space-x-4 bg-gray-800`}
+        >
+            <ul className="flex flex-col md:flex-row">
+                <li className="py-2 md:py-0">
+                    <Link
+                    to="/"
+                    className="block px-4 py-2 hover:bg-gray-700 md:hover:bg-transparent"
+                >
+                    Home
+                    </Link>
+                </li>
+                <li className="py-2 md:py-0">
+                    <Link
+                    to="/about"
+                    className="block px-4 py-2 hover:bg-gray-700 md:hover:bg-transparent"
+                    >
+                    About
+                    </Link>
+                </li>
+                <li className="py-2 md:py-0">
+                    <Link
+                    to="/contact"
+                    className="block px-4 py-2 hover:bg-gray-700 md:hover:bg-transparent"
+                    >
+                    Contact
+                    </Link>
+                </li>
+            </ul>
+        </div>
+        </nav>
+        <Outlet />
+    </>
     );
 };
 
